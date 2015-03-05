eeaccea37810fe63bd24f2094acecf76a176a8d5
Azham <tam2-naphiel@STU-CZC4163CQ7.campus.ads.uwe.ac.uk>
2015-03-05 08:04:36 +0000

    custom 404

--- api/404.php
+++ api/404.php
@@ -2,10 +2,28 @@
 <html>
     <head>
         <title>404!</title>
+        <style>
+
+            a {
+                color: #FFFFFF;
+                 text-decoration: none;
+            }
+
+
+
+        </style>
     </head>
-    <body style="background-image:url('404.jpg'); background-repeat:no-repeat; background-size: cover; ">
-        
-        
+    <body style="background-color:black; ">
+
+        <div style=' margin-left: auto; margin-right: auto; height:700px; width: 1024px; background-image: url("404.jpg"); background-size:cover;'>
+
+            <div style="position:absolute; font-size:36px; margin-top: 450px; margin-left: 250px;  font-family:papyrus;">
+                <a href="#" style="a:link">API Guide</a>
+            </div>
+<!--            <img src="404.jpg" style="z-index:50;">-->
+
+        </div>
+
     </body>
 </html>
 
